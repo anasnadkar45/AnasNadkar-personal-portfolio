bc12b99058c89b1d2063eeb0878d4c868d7ab81d
anasnadkar45 <anasnadkar23@gmail.com>
2024-07-07 22:31:09 +0530

    Updated Codebase

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -13,7 +13,7 @@ export function Navbar() {
     return (
         <nav className="flex justify-between fixed top-2 left-[5vw] right-[5vw] w-[90vw] 
         md:right-[15vw] md:left-[15vw] md:w-[70vw] mx-auto py-2 px-2 bg-gray-700 border 
-        border-[#FCD269] rounded-md backdrop-filter backdrop-blur-sm bg-opacity-10 z-20">
+        border-[#FCD269] rounded-md backdrop-filter backdrop-blur-md bg-opacity-10 z-20">
             <div>
                 <Logo />
             </div>
