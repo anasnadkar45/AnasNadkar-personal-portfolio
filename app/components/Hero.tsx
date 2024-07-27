5b2ed6ba38037bda5796c70b6ef2ab3442ff4937
anasnadkar45 <anasnadkar23@gmail.com>
2024-07-27 17:30:44 +0530

    Fixed Codebase

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -33,7 +33,7 @@ export function Hero() {
                     </p> */}
 
                     <div className="w-[150px] mx-auto">
-                        <Link href={'https://drive.google.com/file/d/1TiTBvf9VNwNGw-B67VdemHDvdRyMWy24/view?usp=drive_link'}>
+                        <Link href={'https://drive.google.com/file/d/1x4YRTsJXhIAWWGgr3Cn59EvN8r_42GxN/view?usp=drive_link'}>
                             <EyeCatchingButton_v1 title="Resume" />
                         </Link>
                     </div>
